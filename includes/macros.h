809a845cb3f4ab67c5d181d5cf35629a97c7f03a
sboukiou <sboukiou@student.1337.ma>
2025-08-06 09:09:16 +0100

    Adding includes for other needed headers (x11 keys header) + Declaring macros for keys/colors and other needed data...

--- includes/macros.h
+++ includes/macros.h
@@ -13,11 +13,31 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+# define SUCCESS 0
 #define FAILURE 1
-#define SUCCESS 0
+# define MAP_SIZE 1024
+# define TRUE 1
+# define FALSE 0
+# define FAIL -1
+# define ESCAPE 65307
+/*Defining Window demensions*/
+# define WIN_HEIGHT 1080
+# define WIN_WIDTH 1920
+/*Defining Colors*/
+# define RED 0xFF0000
+# define WHITE 0xFFFFFF
+# define GREEN 0x00FF00
+# define BLUE 0xFF00FF
+# define DEFAULT_Z 0x99CCFF
+# define DEFAULT 0x00CC66
+/*Defining Macros for events*/
+# define KEY_PRESS_EVENT 2
+# define BTN_PRESS_EVENT 4
+# define DESTROY_NOTIFY_EVENT 17
+
 
 # define UNUSED(X) (void)(X);
-# define PANIC(X) dprintf(STDERR_FILENO, "Err: [%s]\n", X);\
+# define PANIC(X) dprintf(STDERR_FILENO, "Err: [%s]\n", X),\
 	exit(EXIT_FAILURE);
 
 #endif
