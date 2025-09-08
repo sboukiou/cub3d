f01371b44d64e73e900abeaa7d3d94d294641c96
hmouis <houdayfamouis@gmail.com>
2025-09-08 10:50:28 +0100

    CHANGE: map_info to parse

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hmouis <hmouis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 15:21:46 by hmouis            #+#    #+#             */
-/*   Updated: 2025/09/01 09:52:29 by hmouis           ###   ########.fr       */
+/*   Updated: 2025/09/08 10:48:03 by hmouis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	main(int ac, char **av)
 {
-	t_map_info	*map_info;
+	t_map_info	*parse;
 	t_info	*info;
 
 	
-	map_info = NULL;
+	parse = NULL;
 	info = NULL;
 	if (ac != 2)
 		return (printf("ERROR\nmissing file\n"), FAILURE);
-	if (parse_map(av, &map_info, &info) == FAILURE)
+	if (parse_map(av, &parse, &info) == FAILURE)
 		return (FAILURE);
-	printf("max width = %d\n", info->map_height);
+	
 	return (SUCCESS);
 }
--- src/mlx_draw.c
+++ src/mlx_draw.c
@@ -19,32 +19,6 @@ void draw_player(int pos_x, int pos_y, t_mlx_img *img, t_info *info)
 					buffered_mlx_pixel_put(img, i, j, RED);
 }
 
-void draw_walls(t_info *info, t_mlx_img *img)
-{
-	if (info == NULL || img == NULL)
-}
-{
-	int	scale_x;
-	int	scale_y;
-
-	scale_x = (WIN_WIDTH / info->map_width) * pos_x;
-	scale_y = (WIN_HEIGHT / info->map_width) * pos_y;
-
-		for (int j = scale_y; j < scale_y + 23; j++)
-			for (int i = scale_x; i < scale_x + 23; i ++)
-			{
-				if (i < WIN_WIDTH && j < WIN_HEIGHT)
-				{
-					if (color == 0)
-						buffered_mlx_pixel_put(img, i, j, WHITE);
-					else
-						buffered_mlx_pixel_put(img, i, j, PINK);
-				}
-			}
-
-}
-
-
 int mlx_draw_map(t_mlx *mlx, t_info *info)
 {
 	if (info == NULL || mlx == NULL)
@@ -52,7 +26,6 @@ int mlx_draw_map(t_mlx *mlx, t_info *info)
 	for (int i = 0; i < WIN_HEIGHT; i++)
 		for(int j = 0; j < WIN_WIDTH; j++)
 			buffered_mlx_pixel_put(&mlx->img, j, i, 0x16082f);
-	draw_walls(t_info *info, mlx->img);
 	draw_player(info->player_x, info->player_y, &mlx->img, info);
 	mlx_put_image_to_window(mlx->display, mlx->window, mlx->img.img, 0, 0);
 	return (SUCCESS);
