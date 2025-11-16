0840b60099732ca9234b6f0ec40b06f6bdb31574
sboukiou <sboukiou@student.1337.ma>
2025-11-16 02:49:52 +0100

    Added the rgb_merge function that transforms the floor and ceiling colors into unsigned integers

--- src/math.c
+++ src/math.c
@@ -8,3 +8,10 @@ int calculate_distance(int px, int py, int dx, int dy)
     return (int)distance;
 }
 
+unsigned int rgb_merge(int r, int g, int b)
+{
+    if (r < 0) r = 0; if (r > 255) r = 255;
+    if (g < 0) g = 0; if (g > 255) g = 255;
+    if (b < 0) b = 0; if (b > 255) b = 255;
+    return ((unsigned int)r << 16) | ((unsigned int)g << 8) | (unsigned int)b;
+}
