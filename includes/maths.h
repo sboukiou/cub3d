22975a411a3b2a3282fc89c6b57b867698c178be
sboukiou <sboukiou@student.1337.ma>
2025-11-24 16:40:53 +0100

    Added more functions protos for the rendering part

--- includes/maths.h
+++ includes/maths.h
@@ -2,6 +2,11 @@
 # define MATHS_H
 
 
+#include "types.h"
 int calculate_distance(int px, int py, int dx, int dy);
 unsigned int rgb_merge(int r, int g, int b);
+void	calculateVertLine(t_prog *prog);
+void	performeDDA(t_prog *prog);
+void	settingSteps(t_prog *prog);
+void	calculateRayPostion(t_prog *prog, int currentColumn);
 #endif
